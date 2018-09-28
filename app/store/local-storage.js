e957b98b7a7977ca138b6bb9d2dae4980ffee070
Scott <scott.kitchell@myagi.com>
2018-09-28 19:38:55 +1000

    Create a standard LocalStorage service

---
+++ app/store/local-storage.js
@@ -0,0 +1,197 @@
+import { Platform, AsyncStorage } from 'react-native';
+import { find, findIndex, maxBy } from 'lodash';
+
+export default class LocalStorage {
+
+  static _uniqueId = (dataArray) => {
+    const latest = maxBy(dataArray, 'id');
+    return latest? latest.id+1 : 0;
+  }
+
+  static get = (key, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else {
+          const dataArray = data? JSON.parse(data) : [];
+          if(resCB) resCB(dataArray, null);
+          resProm(dataArray);
+        }
+      });
+    });
+  }
+
+  static getOne = (key, id, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else if(data) {
+          const dataItem = data? find(JSON.parse(data),{id}) : null;
+          if(resCB) resCB(dataItem, null);
+          resProm(dataItem)
+        }
+      });
+    });
+  }
+
+  static create = (key, dataItem, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else {
+          let dataArray = data? JSON.parse(data) : [];
+          dataItem.id = LocalStorage._uniqueId(dataArray);
+          dataArray.unshift(dataItem);
+          AsyncStorage.setItem(key, JSON.stringify(dataArray), (error) => {
+            if(error) {
+              if(resCB) resCB(dataArray, error);
+              errProm(error);
+            } else {
+              if(resCB) resCB(dataArray, null);
+              resProm(dataArray);
+            }
+          });
+        }
+      });
+    });
+  }
+
+  // static updateAll(key, dataArray, resCB) {
+  //   return new Promise((resProm, errProm) => {
+  //     AsyncStorage.getItem(key, (error, data) => {
+  //       if(error) {
+  //         if(resCB) resCB(null, error);
+  //         errProm(error);
+  //       } else {
+  //         let storedMemories = data? JSON.parse(data) : [];
+  //         if(dataArray.length < (storedMemories.length-10)) {
+  //           const errorMsg = "Too many deleting changes made to dataArray. Limit the number of dataArray removed to 10 or less.";
+  //           if(resCB) resCB(null, errorMsg);
+  //           errProm(errorMsg);
+  //           return;
+  //         }
+  //         AsyncStorage.setItem(key, JSON.stringify(dataArray), (error) => {
+  //           if(error) {
+  //             if(resCB) resCB(dataArray, error);
+  //             errProm(error);
+  //           } else {
+  //             if(resCB) resCB(dataArray, null);
+  //             resProm(dataArray);
+  //           }
+  //         });
+  //       }
+  //     });
+  //   });
+  // }
+
+  static update = (key, id, dataItem, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else if(data) {
+          let dataArray = JSON.parse(data);
+          const index = findIndex(dataArray,{id});
+          dataArray[index] = dataItem;
+          AsyncStorage.setItem(key, JSON.stringify(dataArray), (error) => {
+            if(error) {
+              if(resCB) resCB(dataArray, error);
+              errProm(error);
+            } else {
+              if(resCB) resCB(dataArray, null);
+              resProm(dataArray);
+            }
+          });
+        } else {
+          error = `UPDATE ERROR: No ${key} item found with id=${id}.`;
+          console.error(error);
+          if(resCB) resCB(null, error);
+          errProm(error);
+        }
+      });
+    });
+  }
+
+  static dangerouslyUpdateAll = (key, dataArray, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else if(data) {
+          if(dataArray.length != JSON.parse(data).length) {
+            error = `UPDATE ALL ERROR: ${key} items provided do not match the stored items.`;
+            console.error(error);
+            if(resCB) resCB(null, error);
+            errProm(error);
+          } else {
+            AsyncStorage.setItem(key, JSON.stringify(dataArray), (error) => {
+              if(error) {
+                if(resCB) resCB(dataArray, error);
+                errProm(error);
+              } else {
+                if(resCB) resCB(dataArray, null);
+                resProm(dataArray);
+              }
+            });
+          }
+        } else {
+          error = `UPDATE ALL ERROR: No ${key} item found with id=${id}.`;
+          console.error(error);
+          if(resCB) resCB(null, error);
+          errProm(error);
+        }
+      });
+    });
+  }
+
+  static delete = (key, id, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.getItem(key, (error, data) => {
+        if(error) {
+          if(resCB) resCB(null, error);
+          errProm(error);
+        } else if(data) {
+          let dataArray = JSON.parse(data);
+          const index = findIndex(dataArray,{id});
+          dataArray.splice(index, 1);
+          AsyncStorage.setItem(key, JSON.stringify(dataArray), (error) => {
+            if(error) {
+              if(resCB) resCB(dataArray, error);
+              errProm(error);
+            } else {
+              if(resCB) resCB(dataArray, null);
+              resProm(dataArray);
+            }
+          });
+        } else {
+          error = `DELETE ERROR: No ${key} item found with id=${id}.`;
+          console.error(error);
+          if(resCB) resCB(null, error);
+          errProm(error);
+        }
+      });
+    });
+  }
+
+  static nuke = (key, resCB) => {
+    return new Promise((resProm, errProm) => {
+      AsyncStorage.removeItem(key, (error) => {
+        if(error) {
+          if(resCB) resCB({}, error);
+          errProm(error);
+        } else {
+          if(resCB) resCB({}, null);
+          resProm({});
+        }
+      });
+    });
+  }
+}
