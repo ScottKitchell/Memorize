c7b6a7819aafe64aa4f5ffe026be8ce8929cdd94
Scott <scott.kitchell@myagi.com>
2019-01-12 15:45:33 +1100

    Update social-text to be more stable

--- app/components/generic/social-text.js
+++ app/components/generic/social-text.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput } from 'react-native';
+import {Text, TextInput} from 'react-native';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
-const URL_REGEX = /^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/
+const URL_REGEX = /^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
 
 
 export class SocialTextInput extends React.Component {
@@ -16,40 +15,61 @@ export class SocialTextInput extends React.Component {
     super(props);
     this.state = {
       cursor: 0,
-    }
+    };
     this.textInputRef = React.createRef();
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    // Only update when cursor has changed as this is after text has been updated
-      // Don't update for cursor changes
-    return (nextState.cursor === this.state.cursor);
+    // Don't update for cursor changes
+    return ((nextState.cursor === this.state.cursor) && (nextProps.value !== this.props.value));
   }
 
   clear = () => {
-    if (this.props.onChangeText) this.props.onChangeText('');
+    if (this.props.onChangeText) this._handleChangeText('');
     else this.textInputRef.current.clear();
   }
 
   isFocused = () => this.textInputRef.current.isFocused();
 
-  insertAtCursor = (chars, cursor=this.state.cursor) => {
+  insertAtCursor = (chars, startIndex=this.state.cursor, endIndex=this.state.cursor) => {
+    // console.log(`insertAtCursor - "${chars}" at ${startIndex}-${endIndex}`);
     if (!this.props.onChangeText) return;
-    const text = this.props.value.slice(0, cursor).concat(chars, this.props.value.slice(cursor));
-    this.props.onChangeText(text);
+    const text = this.props.value.slice(0, startIndex).concat(chars, this.props.value.slice(endIndex));
+    const cursorNowAt = startIndex + chars.length;
+    this._handleChangeText(text, cursorNowAt);
   }
 
-  _handleSelectionChange = (event) => {
+  autoCompleteWord = (chars) => {
+    const cursor = this.state.cursor;
+    console.log(`autoCompleteWord - "${chars}"`);
+    const fromIndex = this._getWordStartIndex(this.props.value, cursor);
+    this.insertAtCursor(chars+" ", fromIndex, cursor);
+  }
+
+  _handleChangeText = (text, cursor=this.state.cursor) => {
+    // console.log(`onChangeText - cursor was at ${cursor}`);
+    this._checkForKeyWords(text, cursor);
+    if (this.props.onChangeText)
+      this.props.onChangeText(text);
+  }
+
+  _handleChangeSelection = (event) => {
+    // console.log(`onChangeCursor - from ${this.state.cursor} to ${event.nativeEvent.selection.start}`);
     this.setState({cursor: event.nativeEvent.selection.start});
     if (this.props.onSelectionChange)
       this.props.onSelectionChange(event);
   }
 
-  _checkForKeyWords = async (text) => {
-    if(!this.props.onHashtagEntering) {
-      const wordStartIndex = this._getWordStartIndex(text, this.state.cursor);
-      if(text[wordStartIndex] === '#')
-        this.props.onHashtagEntering(text.slice(wordStartIndex+1, this.state.cursor+1));
+  _checkForKeyWords = (text, cursor=this.state.cursor) => {
+    if(this.props.onHashtagEntering) {
+      const wordStartIndex = this._getWordStartIndex(text, cursor);
+      if(text[wordStartIndex] === '#') {
+        const tag = text.slice(wordStartIndex+1, cursor+1);
+        // console.log(`onHashtagEntering - "#${tag}"`);
+        this.props.onHashtagEntering(tag);
+      }
+      else
+        this.props.onHashtagEntering(null);
     }
   }
 
@@ -57,18 +77,19 @@ export class SocialTextInput extends React.Component {
     for(let i=cursor; i>=0; i--) {
       if(text[i] === ' ') return i+1;
     }
+    return 0;
   }
 
   render() {
-    const { value, onHashtagEntering, hashtagStyle, urlStyle, ...props } = this.props;
-    this._checkForKeyWords(value);
+    const {value, onHashtagEntering, hashtagStyle, urlStyle, ...props} = this.props;
     return (
       <TextInput
         ref={this.textInputRef}
         {...props}
-        onSelectionChange={this._handleSelectionChange}
+        onChangeText={this._handleChangeText}
+        onSelectionChange={this._handleChangeSelection}
       >
-        <SocialText hashtagStyle={hashtagStyle} urlStyle={urlStyle}>
+        <SocialText hashtagStyle={hashtagStyle} urlStyle={urlStyle} shortUrl={false}>
           {value}
         </SocialText>
       </TextInput>
@@ -78,25 +99,35 @@ export class SocialTextInput extends React.Component {
 
 
 export function SocialText(props) {
-  const { onHashtagPress, onURLPress, hashtagStyle, urlStyle, ...containerProps } = props;
+  const shortenUrl = (url) => {
+    const domain = url.replace(/(^\w+:|^)\/\//, '');
+    return (domain.lengh <= 28) ? domain : domain.slice(0, 24) + "...";
+  };
+  const {onHashtagPress, onUrlPress, hashtagStyle, urlStyle, ...containerProps} = props;
   const richText = props.children.split(" ").reduce((textArray, word, i) => {
     const richWord = (word[0] === "#")
-      ? <Text onPress={()=>props.onHashtagPress(word)} key={i} style={hashtagStyle}>{word}</Text>
+      ? <Text onPress={() => onHashtagPress(word)} key={i} style={hashtagStyle}>{word}</Text>
       : (word.match(URL_REGEX))
-        ? <Text onPress={()=>props.onUrlPress(word)} key={i} style={urlStyle}>{word}</Text>
+        ? <Text onPress={() => onUrlPress(word)} key={i} style={urlStyle}>
+          {props.shortUrl ? shortenUrl(word): word}
+        </Text>
         : word;
     return textArray.concat((i)? [' ', richWord] : [richWord]);
   }, []);
   return <Text {...containerProps}>{richText}</Text>;
 }
+
+
 SocialText.propTypes = {
   ...Text.propTypes,
   hashtagStyle: Text.propTypes.style,
   urlStyle: Text.propTypes.style,
   onHashtagPress: PropTypes.func,
   onUrlPress: PropTypes.func,
-}
-SocialText.defaultTypes = {
+  shortUrl: PropTypes.bool,
+};
+SocialText.defaultProps = {
   onHashtagPress: ()=>true,
   onUrlPress: ()=>true,
-}
+  shortUrl: true,
+};
